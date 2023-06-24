fa5492ee63f37c7be0983b88d553352815ae5369
taishira <19t2083j@shinshu-u.ac.jp>
2023-06-24 19:05:21 +0900

    Co-authored-by: ayame113 <ayame113@users.noreply.github.com> Co-authored-by: hino <Cyndaquil1999@users.noreply.github.com>

--- server.mjs
+++ server.mjs
@@ -7,8 +7,12 @@ import { config } from 'dotenv'
 
 config();
 
-const questionJson = await fs.readFile(path.resolve(path.dirname(fileURLToPath(import.meta.url)),'question.json'), 'utf-8');
+const questionJson = await fs.readFile(path.resolve(path.dirname(fileURLToPath(import.meta.url)),'questions.json'), 'utf-8');
 const questionData = JSON.parse(questionJson)
+const length = questionData.length;
+
+
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_KEY, // replace with your actual OpenAI key
@@ -31,16 +35,18 @@ app.use(express.urlencoded({
 }));
 
 app.get("/pushQuestion", async (req, res) => {
-  res.json({ question: questionData[0].question });
+  const randomNumber = Math.floor(Math.random() * length);
+  res.json({ question: questionData[randomNumber].question ,questionIndex:randomNumber});
 });
 
 app.post("/umigame", async (req, res) => {
   console.log(req.body);
+  const {questionIndex} = req.body
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [{
       role: `system`,
-      content:`${questionData[0].prompt}\n質問に対して、「はい」か「いいえ」か「わかりません」とだけで答えて下さい`
+      content:`${questionData[questionIndex].prompt}\n質問に対して、「はい」か「いいえ」か「わかりません」とだけで答えて下さい`
     },{
       role: 'user',
       content: req.body.prompt
@@ -51,8 +57,9 @@ app.post("/umigame", async (req, res) => {
 
 app.post("/answer", async (req, res) => {
   //console.log(req.body);
+  const {questionIndex} = req.body
   const userAnswer = req.body.AnswerStr;
-  const realAnswer = questionData[0].answer;
+  const realAnswer = questionData[questionIndex].answer;
   console.log({ userAnswer, realAnswer });
   const response = await openai.createChatCompletion({
     // model: 'gpt-3.5-turbo-0613',
@@ -65,7 +72,14 @@ app.post("/answer", async (req, res) => {
   res.json({ answer: response.data.choices[0].message.content });
 });
 
-app.listen(3001, () => {
-  console.log("listening http://localhost:3001/")
+app.listen(3000, () => {
+  console.log("listening http://localhost:3000/")
 });
 
+function getPointFromSentence(text) {
+  const result = text.match(/\d+\.?\d点/g);
+  if (result && result.length) {
+    return result[0]
+  }
+  return "0点"
+}
